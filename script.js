fe07a4cb04465a75147f91c034aaf198ff79a454
Rujuta Jagtap <rujuta.jagtap16996@gmail.com>
2023-04-22 12:28:51 +0530

    myportfolio

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("script is running.....")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+          document.querySelector('.sidebar').classList.toggle('sidebarGo');
+         if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+              document.querySelector('.ham').style.display='inline'
+              document.querySelector('.cross').style.display='none'
+}
+else{
+    document.querySelector('.ham').style.display='none'
+    setTimeout(() => {
+    document.querySelector('.cross').style.display='inline'
+    },350);
+}
+
+})
+
+    /* for add more button functionality */
+// function myFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("myBtn");
+  
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
